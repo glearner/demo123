e73df9dab888886923abebb775518a27d76aa609
gaurav <gaurav.verma@piktorlabs.com>
2020-10-06 15:54:00 +0530

    added auth

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,9 @@ import { useDispatch, useSelector } from "react-redux";
 const Home = () => {
 	const tokenStr = useSelector((state) => console.log(state));
 	useEffect(() => {
-		axios.get("/sample_vendor/validate​").then((data) => console.log(data));
+		axios
+			.get("/sample_vendor/validate/?nitems=1")
+			.then((data) => console.log(data));
 	});
 	return <div>Home</div>;
 };
--- src/index.js
+++ src/index.js
@@ -13,13 +13,12 @@ import * as serviceWorker from "./serviceWorker";
 
 axios.defaults.baseURL =
 	"https://assistant-qa-server-dot-vera-188811.uc.r.appspot.com";
-axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 axios.interceptors.request.use(
 	(request) => {
 		const token = localStorage.getItem("token");
-		request.headers.Authorization = token;
+		request.headers.Authorization = "Bearer " + token;
 		return request;
 	},
 	(error) => {
